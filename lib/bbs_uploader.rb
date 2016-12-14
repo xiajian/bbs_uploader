b26266b633a2b446e639ddcd2840449f16a75cca
xiajian <jhqy2011@gmail.com>
2016-12-14 18:03:43 +0800

    Fix bug and add file exist verify

--- lib/bbs_uploader.rb
+++ lib/bbs_uploader.rb
@@ -6,14 +6,18 @@ require "bbs_uploader/ruby_regex"
 
 module BbsUploader
   # Your code goes here...
-  IMAGE_URL_REGEXP = /http:\/\/.*[jpg|png|jpeg]$/
+  IMAGE_URL_REGEXP = /http:\/\/.*(jpg|png|jpeg)$/
 
-  IMAGE_FILE_REGEXP = /.*[jpg|png|jpeg|gif|bmp]$/
+  IMAGE_FILE_REGEXP = /.*(jpg|png|jpeg|gif|bmp)$/
 
   module FileType
     IMAGE = 'image'.freeze
 
     FILE = 'file'.freeze
+  end
+  
+  module Code
+    SUCCESS = 200
   end
 
   class << self
@@ -71,35 +75,49 @@ module BbsUploader
       ''
     end
 
-    def upload_file(file_path, file_type = FileType::FILE)
-      Qiniu.establish_connection! access_key: self.qiniu[:access_key],
-                                  secret_key: self.qiniu[:secret_key]
+    # 返回文件的状态码
+    def get_file_info(key)
+      establish_connection
+      
+      # 获取文件信息
+      code, result, response_headers = Qiniu::Storage.stat(
+          self.qiniu[:bucket], # 存储空间
+          key # 资源名
+      )
+      
+      code
+    end
 
+    def upload_file(file_path, file_type = FileType::FILE)
       key = if file_type == FileType::FILE
               "bbs_file/#{File.basename(file_path)}"
             elsif file_type == FileType::IMAGE
               "bbs_image/#{File.basename(file_path)}"
             end
+              
+      unless (code = get_file_info(key)) == Code::SUCCESS
+        establish_connection
+        
+        put_policy = Qiniu::Auth::PutPolicy.new(
+            self.qiniu[:bucket], # 存储空间
+            key, # 指定上传的资源名，如果传入 nil，就表示不指定资源名，将使用默认的资源名
+            3600 # token 过期时间，默认为 3600 秒，即 1 小时
+        )
 
-      put_policy = Qiniu::Auth::PutPolicy.new(
-          self.qiniu[:bucket], # 存储空间
-          key, # 指定上传的资源名，如果传入 nil，就表示不指定资源名，将使用默认的资源名
-          3600 # token 过期时间，默认为 3600 秒，即 1 小时
-      )
+        uptoken = Qiniu::Auth.generate_uptoken(put_policy)
 
-      uptoken = Qiniu::Auth.generate_uptoken(put_policy)
-
-      # 调用 upload_with_token_2 方法上传
-      code, result = Qiniu::Storage.upload_with_token_2(
-          uptoken,
-          file_path,
-          key,
-          nil, # 可以接受一个 Hash 作为自定义变量，请参照 http://developer.qiniu.com/article/kodo/kodo-developer/up/vars.html#xvar
-          bucket: self.qiniu[:bucket]
-      )
-
-      if code == 200
-        file_url = "http://#{self.qiniu[:bucket_domain]}/#{result['key']}"
+        # 调用 upload_with_token_2 方法上传
+        code, result = Qiniu::Storage.upload_with_token_2(
+            uptoken,
+            file_path,
+            key,
+            nil, # 可以接受一个 Hash 作为自定义变量，请参照 http://developer.qiniu.com/article/kodo/kodo-developer/up/vars.html#xvar
+            bucket: self.qiniu[:bucket]
+        )
+      end
+      
+      if code == Code::SUCCESS
+        file_url = "http://#{self.qiniu[:bucket_domain]}/#{key}"
 
         puts "上传成功! \n链接为: #{file_url}"
 
@@ -194,6 +212,13 @@ module BbsUploader
       elsif options[:input_directory]
         generate_markdown_format options[:input_directory]
       end
+    end
+    
+    protected
+    
+    def establish_connection
+      Qiniu.establish_connection! access_key: self.qiniu[:access_key],
+                                  secret_key: self.qiniu[:secret_key]
     end
   end
 end
